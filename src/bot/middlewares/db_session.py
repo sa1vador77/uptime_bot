b6c54d4f38ee4718b55574a51f56c073f0b8127c
sa1vador77 <uncrowned.blessed@gmail.com>
2026-01-17 05:51:39 +0300

    Написан миддлварь для получения контекстной сессии

---
+++ src/bot/middlewares/db_session.py
@@ -0,0 +1,44 @@
+from typing import Callable, Awaitable, Any
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+
+from src.infrastructure.database.repos.monitors_repo import MonitorRepository
+
+
+class DbSessionMiddleware(BaseMiddleware):
+    """
+    Миддлварь для управления сессией подключения к БД.
+    Берет async_sessionmaker из диспетчера, создает сессию и экземпляр MonitorRepository.
+    """
+    def __init__(self, session_factory: async_sessionmaker[AsyncSession]):
+        super().__init__()
+        self.session_factory = session_factory
+
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict[str, Any],
+    ) -> Any:
+        async with self.session_factory() as session:
+            # Создаем репозиторий с текущей сессией
+            repo = MonitorRepository(session)
+            
+            # Прокидываем репозиторий в handler
+            data["repo"] = repo
+            
+            try:
+                # Вызываем хендлер
+                result = await handler(event, data)
+                
+                # Если хендлер отработал без ошибок — коммитим транзакцию
+                await session.commit()
+                return result
+                
+            except Exception:
+                # Если была ошибка — откатываем изменения
+                await session.rollback()
+                raise
