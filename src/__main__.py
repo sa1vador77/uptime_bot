1eaef789cddf2318f7b08930c674ce19a2487460
sa1vador77 <uncrowned.blessed@gmail.com>
2026-01-19 01:40:18 +0300

    Обновлен головной файл

--- src/__main__.py
+++ src/__main__.py
@@ -6,21 +6,22 @@ from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
 from src.core.config import settings
-from src.core.logger import setup_logger
+from src.core.logger import configure_logger
+from src.bot.middlewares import DbSessionMiddleware, LoggingMiddleware
 from src.infrastructure.database.manager import db_manager
-from src.bot.middlewares.db_session import DbSessionMiddleware
-# from src.bot.handlers import user_router
+
+from src.bot.handlers import user_router
 
 
 async def main():
     # 1. Настройка логгера
-    setup_logger()
+    configure_logger()
     logger.info("Запуск приложения...")
 
     # 2. Инициализация бота
     bot = Bot(
         token=settings.BOT_TOKEN.get_secret_value(),
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
 
     # 3. Инициализация диспетчера
@@ -28,17 +29,18 @@ async def main():
 
     # 4. Подключение Middleware
     # Передаем фабрику сессий в мидлварь
+    dp.update.outer_middleware(LoggingMiddleware())
     dp.update.middleware(DbSessionMiddleware(session_factory=db_manager.session_maker))
 
     # 5. Регистрация роутеров
-    # dp.include_router(user_router)
+    dp.include_router(user_router)
 
     # 6. Запуск polling
     try:
         # Удаляем вебхук и дропаем накопившиеся апдейты (чтобы бот не отвечал на старое)
         await bot.delete_webhook(drop_pending_updates=True)
-        logger.info("Бот начал прослушивание событий")
         await db_manager.health_check()
+        logger.info("Бот начал прослушивание событий")
         await dp.start_polling(bot)
     finally:
         logger.info("Остановка приложения...")
@@ -48,7 +50,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        logger.info("Бот остановлен вручную")
+    asyncio.run(main())
